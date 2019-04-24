75b40d01087f21fbac29dc58f49eeb5529690136
Robert He <robert.he.19@dartmouth.edu>
2019-04-24 00:14:04 -0400

    finished workshop

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.bd42feedd799c41150a1a231a532d281.js"
 );
 
 workbox.clientsClaim();
